8dd0d06b2e906fc562d34bc637da43793a90e374
martin2387 <cheenote@hotmail.com>
2013-08-15 14:30:43 -0400

    Here's your racial sandwich program Clyde!!

--- src/Sandwich.java
+++ src/Sandwich.java
@@ -1,16 +1,35 @@
 
 public class Sandwich {
-
-	/* 
-	 * This is the Sandwich class.
-	 * This is an object class.
-	 * There will be no main() method here
-	 * Instead, you will declare variables to create
-	 * the object's "fields", and then add methods 
-	 * to retrieve or change the value of each field.
-	 * 
-	 */
-
-
+	
+	//Declare fields for this Object
+	static String mainIngred;
+	static String breadType;
+	static double price;
+	
+	//Declare access methods
+	public String getMainIngred() {
+		return mainIngred;
+	}
+	
+	public String getBreadType() {
+		return breadType;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	//Declare mutator methods
+	public void setMainIngred(String newMainIngred) {
+		mainIngred = newMainIngred;
+	}
+	
+	public void setBreadType(String newBreadType) {
+		breadType = newBreadType;
+	}
+	
+	public void setPrice(double newPrice) {
+		price = newPrice;
+	}	 
 
 }
--- src/TestSandwich.java
+++ src/TestSandwich.java
@@ -1,21 +1,36 @@
+import javax.swing.JOptionPane;
 
 public class TestSandwich {
 
 	public static void main(String[] args) {
-		/*
-		 * This class is a program class.
-		 * It WILL include a main() method.
-		 * This program should create two instances of
-		 * Sandwich, and it should demonstrate the
-		 * use of all of the methods encapsulated
-		 * in the Sandwich class.
-		 * 
-		 * Finish off this program by displaying the
-		 * values of each of the fields for each 
-		 * Sandwich instance.
-		 * 
-		 */
-
+		
+		//Instantiate a Sandwich object
+		Sandwich megaSandwich = new Sandwich();
+		Sandwich bigSandwich = new Sandwich();
+		
+		//Set the values of fields in megaSandwich
+		megaSandwich.setMainIngred("pepperoni");
+		megaSandwich.setBreadType("white");
+		megaSandwich.setPrice(10.00);
+		
+		//Output the info of megaSandwich
+		String customer;
+		customer = JOptionPane.showInputDialog("Who wants a mega sandwich? ");
+		JOptionPane.showMessageDialog(null, customer + " wants a " + megaSandwich.getMainIngred() +
+				" sandwich with " + megaSandwich.getBreadType() + " bread. This will cost $" + 
+				megaSandwich.getPrice() + " dollars.");
+		 
+		//Set the values of fields in bigSandwich
+		bigSandwich.setMainIngred("salami");
+		bigSandwich.setBreadType("whole wheat");
+		bigSandwich.setPrice(8.00);
+		
+		//Output the info of bigSandwich
+		customer = JOptionPane.showInputDialog("Who wants a big sandwich? ");
+		JOptionPane.showMessageDialog(null, customer + " wants a " + bigSandwich.getMainIngred() +
+				" sandwich with " + bigSandwich.getBreadType() + " bread. This will cost $" + 
+				bigSandwich.getPrice() + " dollars.");
+		 
 	}
 
 }
